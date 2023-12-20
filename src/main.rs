bd41c438bf3db34698d76920ec1223bc13c0d717
Thomas ANDONOFF <thomas.andonoff@gmail.com>
2023-12-20 16:12:33 +0100

    Résolution des conflits

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use rand_core::OsRng;
 use crypto_mob::{EphemeralSecret, PublicKey};
 
 use aes_gcm::{
-    aead::{Aead, AeadCore, KeyInit},
-    Aes256Gcm, Key // Or `Aes128Gcm`
+    aead::{Aead, AeadCore, KeyInit, consts::{B1, B0}, generic_array::GenericArray},
+    Aes256Gcm, Key, AesGcm, aes::{Aes256, cipher::typenum::{UInt, UTerm}}
 };
+
+type CipherType = AesGcm<Aes256, UInt<UInt<UInt<UInt<UTerm, B1>, B1>, B0>, B0>>;
+type NonceType = GenericArray<u8, UInt<UInt<UInt<UInt<UTerm, B1>, B1>, B0>, B0>>;
 
 use crypto_mob::hash;
 
@@ -13,54 +16,74 @@ use crypto_mob::hash;
 
 
 fn main() {
-
-
     println!("Starting key exchange!");
+
     let alice_sec = EphemeralSecret::random_from_rng(OsRng);
     let alice_pub = PublicKey::from(&alice_sec);
-
     println!("Alice public key : {:?}", alice_pub.as_bytes());
 
     let bob_sec = EphemeralSecret::random_from_rng(OsRng);
     let bob_pub = PublicKey::from(&bob_sec);
-
     println!("Bob public key : {:?}", bob_pub.as_bytes());
 
     let alice_shared_sec = alice_sec.diffie_hellman(&bob_pub);
     let bob_shared_sec = bob_sec.diffie_hellman(&alice_pub);
-
     let shared_key: &[u8; 32] = alice_shared_sec.as_byte();
-
-    let shared_key_slice: &[u8] = &*shared_key;
-
     assert_eq!(alice_shared_sec.as_byte(), bob_shared_sec.as_byte());
     println!("Shared secret is the same : {:?}", shared_key);
 
-    println!("\n\n");
+    println!("\n\nChiffrement :");
+    let cipher = create_cipher(shared_key);
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
 
+    let ciphertext = encrypt(&cipher, nonce, b"plaintext message");
+    println!("Message chiffré : {:?}", ciphertext);
     
-    // Note that you can get byte array from slice using the `TryInto` trait:
-    //let key: &[u8] = &[42; 32];
+    let plaintext = decrypt(&cipher, nonce, ciphertext);
+    println!("Message déchiffré : {:?}", plaintext);
+
+    println!("Doit correspondre à : {:?}", b"plaintext message");
+    assert_eq!(&plaintext, b"plaintext message");
+
+
+    println!("\n\n\nScalar :");
+    let alice_sec_scalar = alice_sec.get_scalar();    
+    println!("alice_sec_scalar = {:?}",alice_sec_scalar.to_bytes());
+    let bob_sec_scalar =bob_sec.get_scalar();
+    println!("bob_sec_scalar = {:?}",bob_sec_scalar.to_bytes());
+    
+    println!("\n\nsecret_shared_scalar = {:?}",secret_scalar.to_bytes());
+    let message: &[u8] = b"This is a test of the tsunami alert system.";
+
+    println!("Message : {:?}",message);
+
+    println!("\n\nHashing :");
+    let all_data: &[u8] = b"To really appreciate architecture, you may even need to commit a murder."
+    let result = hash(all_data);
+    println!("Hash de dall_data : {:x}",result.to_bytes());
+
+}
+
+
+
+
+pub fn create_cipher(shared_key : &[u8]) -> CipherType{
+    let shared_key_slice: &[u8] = &*shared_key;
     let key: [u8; 32] = match shared_key_slice.try_into() {
         Ok(value) => value,
         Err(error) => {
             eprintln!("Error: {:?}", error);
             // Gérer l'erreur de manière appropriée
             std::process::exit(1);
-
         }
     };
-
-    println!("key : {:?}",key);
-
-    // Alternatively, the key can be transformed directly from a byte slice
-    // (panicks on length mismatch):
     let key = Key::<Aes256Gcm>::from_slice(&key);
-    
     let cipher = Aes256Gcm::new(&key);
-    let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
+    cipher
+}
 
-    let ciphertext_result = cipher.encrypt(&nonce, b"plaintext message".as_ref());
+pub fn encrypt(cipher : &CipherType, nonce : NonceType, plaintext : &[u8]) -> Vec<u8>{
+    let ciphertext_result = cipher.encrypt(&nonce, plaintext.as_ref());
     let ciphertext = match ciphertext_result {
         Ok(value) => value,
         Err(error) => {
@@ -68,9 +91,10 @@ fn main() {
             std::process::exit(1);
         }
     };
+    ciphertext
+}
 
-    println!("Message encrypté : {:?}", ciphertext);
-    
+pub fn decrypt(cipher : &CipherType, nonce : NonceType, ciphertext : Vec<u8>) -> Vec<u8> {
     let plaintext_result = cipher.decrypt(&nonce, ciphertext.as_ref());
     let plaintext = match plaintext_result {
         Ok(value) => value,
@@ -79,35 +103,6 @@ fn main() {
             std::process::exit(1);
         }
     };
-
-    println!("Message décrypté : {:?}", plaintext);
-
-    println!("Doit correspondre à : {:?}", b"plaintext message");
-
-    assert_eq!(&plaintext, b"plaintext message");
-
-    
-    println!("\n\n\nScalar :");
-    let alice_sec_scalar = alice_sec.get_scalar();    
-    println!("alice_sec_scalar = {:?}",alice_sec_scalar.to_bytes());
-    let bob_sec_scalar =bob_sec.get_scalar();
-    println!("bob_sec_scalar = {:?}",bob_sec_scalar.to_bytes());
-
-    let alice_pub_scalar = alice_pub.get_scalar();
-    println!("\n\nalice_pub_scalar = {:?}",alice_pub_scalar.to_bytes());
-    let bob_pub_scalar = bob_pub.get_scalar();
-    println!("bob_pub_scalar = {:?}",bob_pub_scalar.to_bytes());
-
-    let secret_scalar = alice_shared_sec.get_scalar();
-    println!("\n\nsecret_shared_scalar = {:?}",secret_scalar.to_bytes());
-
-
-    let all_data: &[u8] = b"To really appreciate architecture, you may even need to commit a murder."
-    let result = hash(all_data);
-    println!("Hash de dall_data : {:x}",result.to_bytes());
-
-
-
 }
 
     // println!("Starting key exchange!");
