11fbe1940acb60867c87319d1b4f1c2086498cb0
mathbrox <mathieu.pene@gmail.com>
2024-01-16 20:54:31 +0100

    signature qui fonctionne

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 use curve25519_dalek::{edwards::EdwardsPoint, scalar::Scalar, constants::ED25519_BASEPOINT_POINT};
 use rand_core::{RngCore, CryptoRng, OsRng};
 
@@ -11,16 +13,16 @@ fn random_scalar<T: RngCore + CryptoRng>(mut csprng: T) -> Scalar {
 }
 
 /// Generate a random `EdwardPoint`
-fn random_point() -> EdwardsPoint {
+fn generate_keypair() -> (Scalar, EdwardsPoint) {
     let r_scalar = random_scalar(OsRng);
-    r_scalar * ED25519_BASEPOINT_POINT
+    (r_scalar, r_scalar * ED25519_BASEPOINT_POINT)
 }
 
 fn edpoint_to_bytes(edp: EdwardsPoint) -> [u8;32] {
     edp.to_montgomery().to_bytes()
 }
 
-fn hash(digest: Vec<EdwardsPoint>){
+fn hash(digest: Vec<EdwardsPoint>) -> Scalar{
     let v: Vec<[u8;32]> = digest.iter().map(|p| edpoint_to_bytes(*p)).collect();
 
     let mut hasher = <Sha256 as Digest>::new();
@@ -30,15 +32,31 @@ fn hash(digest: Vec<EdwardsPoint>){
 
     let result = hasher.finalize();
     println!("Binary hash: {:?}", result);
+    Scalar::from_bytes_mod_order(result.into())
 }
 
 fn main() {
-    let R = random_point();
+
+    let (x,X) = generate_keypair();
+    let (y,Y) = generate_keypair();
+    let (r,R) = generate_keypair();
     let g = ED25519_BASEPOINT_POINT;
 
-    let b_pk = random_point();
+    let (b_sk, b_pk) = generate_keypair();
 
-    let v: Vec<EdwardsPoint> = vec![R, g, b_pk];
+    let concat_tohash: Vec<EdwardsPoint> = vec![R, g, b_pk, X, Y];
 
-    hash(v);
+    let c = hash(concat_tohash);
+
+    let z = r + c*b_sk;
+
+    let R_prime = z*g - c*b_pk ;
+
+    let concat_tohash_prime = vec![R_prime, g, b_pk, X, Y];
+
+    let c_prime = hash(concat_tohash_prime);
+
+    assert_eq!(c,c_prime);
+
+
 }
